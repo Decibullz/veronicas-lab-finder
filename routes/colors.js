01e31823f65b9620f0d2dec4d227d8edd351e28b
Cody Snell <codyjsnell@yahoo.com>
2020-11-17 11:17:27 -0600

    adds color routes

---
+++ routes/colors.js
@@ -0,0 +1,12 @@
+const express=require(`express`)
+const router = express.Router()
+const colorCtrl = require(`../controllers/colors`)
+
+router.get(`/purple`, colorCtrl.purple)
+router.get(`/green`, colorCtrl.green)
+router.get(`/red`, colorCtrl.red)
+router.get(`/blue`, colorCtrl.blue)
+router.get(`/lined`, colorCtrl.lined)
+router.get(`/swab`, colorCtrl.swab)
+
+module.exports = router
