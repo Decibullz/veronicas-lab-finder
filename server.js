5061cd97b394e79cd2ca76843331e9b1543d7030
Cody Snell <codyjsnell@yahoo.com>
2020-11-18 15:50:33 -0600

    fixes port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // require modules
-const port = 3000
+const port = process.env.PORT || 3000
 const express = require(`express`)
 const morgan = require(`morgan`)
 const methodOverride = require('method-override')
